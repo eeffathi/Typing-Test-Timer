f099e1130c1ba7db27b9958de612d35f5f1d095b
Edris <eeffathi@yahoo.com>
2020-08-20 00:46:00 +0430

    adding eventListener for matching texts

--- script.js
+++ script.js
@@ -5,30 +5,20 @@ const ORIGIN_TEXT = document.querySelector("#origin-text p");
 
 
 
-
-
-
-
 /* functions */
 function start() {
-    // let testAreaLength = TEST_AREA.value.length;
+    let testAreaLength = TEST_AREA.value.length;
     console.log(testAreaLength);
 }
 
-
-
-
-
-
-
-
-
-
-
-
-
+function matchTexts() {
+    let testAreaValue = TEST_AREA.value;
+    console.log(testAreaValue);
+}
 
 
 
 /* EventListeners */
 TEST_AREA.addEventListener("keypress", start, false);
+TEST_AREA.addEventListener("keyup", matchTexts, false);
+BTN_RESET.addEventListener("click", clickBtn, false);
