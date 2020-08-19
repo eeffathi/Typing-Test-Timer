bdaae75b92023e6f7952d6090b247305139c9392
Edris <eeffathi@yahoo.com>
2020-08-20 03:31:52 +0430

    creating the timer

--- script.js
+++ script.js
@@ -1,14 +1,32 @@
 const TEST_AREA = document.querySelector("#test-area");
 const BTN_RESET = document.querySelector("#reset");
 const ORIGIN_TEXT = document.querySelector("#origin-text p");
+const TIMER_TEXTFIELD = document.querySelector(".timer");
 
-
+var timer = [0,0,0,0];
 
 
 /* functions */
+function timerFunction() {
+    // timer++;
+    
+    let currentTime = timer[0] + ":" + timer[1] + ":" + timer[2];
+    TIMER_TEXTFIELD.innerHTML = currentTime;
+
+    timer[3]++;
+
+    timer[0] = Math.floor((timer[3]/100)/60);
+    timer[1] = Math.floor((timer[3]/100) - (timer[0] * 60));
+    timer[2] = Math.floor(timer[3] - (timer[1] * 100) - (timer[0] * 6000));
+}
+
 function start() {
     let testAreaLength = TEST_AREA.value.length;
-    console.log(testAreaLength);
+    // console.log(testAreaLength);
+    // debugger;
+    if(testAreaLength == 0) {
+        setInterval(timerFunction, 10);
+    }
 }
 
 function matchTexts() {
